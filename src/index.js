e448c92f33539c379bf02421d7e26ae4e327ebb9
Irina Shebarshina <iv.shebarshina@gmail.com>
2022-10-01 16:47:46 +0300

    section offer

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
-import 'normalize.css'  // npm install normalize.css
-import './styles/main.scss'
+import 'normalize.css';  // npm install normalize.css
+
+ // core version + navigation, pagination modules:
+ import Swiper, { Navigation, Pagination } from 'swiper';
+ // import Swiper and modules styles
+ import 'swiper/css';
+ import 'swiper/css/navigation';
+ import 'swiper/css/pagination';
+
+import './styles/main.scss';
+
+ // init Swiper:
+ var swiper = new Swiper(".offerSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    slidesPerGroup: 3,
+    loop:true,
+    mousewheel: true,
+    loopFillGroupWithBlank: true,
+    keyboard: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    modules: [Navigation, Pagination]
+  });
