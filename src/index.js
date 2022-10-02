df0c364db5f81adafa12b24c9fde6920510966cb
Irina Shebarshina <iv.shebarshina@gmail.com>
2022-10-02 13:46:10 +0300

    trends

--- src/index.js
+++ src/index.js
@@ -10,22 +10,44 @@ import 'normalize.css';  // npm install normalize.css
 import './styles/main.scss';
 
  // init Swiper:
- var swiper = new Swiper(".offerSwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    freeMode: true,
-    slidesPerGroup: 3,
-    loop:true,
-    mousewheel: true,
-    loopFillGroupWithBlank: true,
-    keyboard: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+var swiper = new Swiper(".offerSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  slidesPerGroup: 3,
+  loop:true,
+  mousewheel: true,
+  loopFillGroupWithBlank: true,
+  keyboard: true,
+  spaceBetween: 25,
+  speed: 800,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
     },
-    modules: [Navigation, Pagination]
-  });
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  modules: [Navigation, Pagination]
+});
+
+var swiper = new Swiper(".trendsSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  freeMode: true,
+  slidesPerGroup: 3,
+  loop:true,
+  mousewheel: true,
+  loopFillGroupWithBlank: true,
+  keyboard: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  modules: [Navigation, Pagination]
+});
