91405cf672d4f77d775742f6324f4217a0f2042c
Irina Shebarshina <iv.shebarshina@gmail.com>
2022-10-12 14:35:22 +0300

    offer swiper adaptive

--- src/index.js
+++ src/index.js
@@ -7,31 +7,44 @@ import 'normalize.css';  // npm install normalize.css
  import 'swiper/css/navigation';
  import 'swiper/css/pagination';
 
-import './styles/main.scss';
+ import './styles/main.scss';
 
  // init Swiper:
-var swiper = new Swiper(".offerSwiper", {
-  slidesPerView: 3,
+var swiper1 = new Swiper(".offerSwiper", {
+  slidesPerView: "auto",
   freeMode: true,
-  slidesPerGroup: 3,
   loop:true,
   mousewheel: true,
   loopFillGroupWithBlank: true,
   keyboard: true,
-  spaceBetween: 25,
+  spaceBetween: 14,
   speed: 800,
+  watchOverflow: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
     },
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
+    clickable: true
   },
-  modules: [Navigation, Pagination]
+  modules: [Navigation, Pagination],
+  breakpoints: {
+    // when window width is >= 300px
+    400: {
+      slidesPerView: 2,
+      // slidesPerGroup: 2,
+      spaceBetween: 25
+    },
+    900: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 25
+    }
+  }
 });
 
-var swiper = new Swiper(".trendsSwiper", {
+var swiper2 = new Swiper(".trendsSwiper", {
   slidesPerView: 3,
   spaceBetween: 20,
   freeMode: true,
@@ -40,6 +53,7 @@ var swiper = new Swiper(".trendsSwiper", {
   mousewheel: true,
   loopFillGroupWithBlank: true,
   keyboard: true,
+  watchOverflow: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -51,12 +65,7 @@ var swiper = new Swiper(".trendsSwiper", {
   modules: [Navigation, Pagination]
 });
 
-// $('.header__burger').click(function() {
-//   $(this).toggleClass('header__burger-close');
-// })
-
 let burger = document.querySelector('.header__burger');
 burger.addEventListener("click", function() {
   this.classList.toggle("burger_close");
-  // burger.style.backgroundColor  = 'red';
 })
